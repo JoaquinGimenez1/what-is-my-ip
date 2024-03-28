2372a0dedd6d0aee6fef3c61cbe89c9682c98732
Joaquin Gimenez <jg@joaquingimenez.com>
2024-03-28 17:33:12 -0500

    Add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -9,29 +9,39 @@
  */
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
   // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-  //
-  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-  // MY_QUEUE: Queue;
+  RATE_LIMITER: DurableObjectNamespace;
+}
+
+interface RateLimiterResponse {
+  milliseconds_to_next_request: number;
 }
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    // Get request headers
-    const headers = new Map(request.headers);
+    // Determine the IP address of the client
+    const ip = request.headers.get('CF-Connecting-IP');
+    if (ip === null) {
+      return new Response(JSON.stringify({ error: 'Could not determine client IP' }, null, 2), { status: 400 });
+    }
+
+    // Obtain an identifier for a Durable Object based on the client's IP address
+    const id = env.RATE_LIMITER.idFromName(ip);
+
+    try {
+      const stub = env.RATE_LIMITER.get(id);
+      const response = await stub.fetch(request);
+      const { milliseconds_to_next_request } = (await response.json()) as RateLimiterResponse;
+      if (milliseconds_to_next_request > 0) {
+        // Alternatively one could sleep for the necessary length of time
+        return new Response(JSON.stringify({ error: 'Rate limit exceeded' }, null, 2), { status: 429 });
+      }
+    } catch (error) {
+      return new Response(JSON.stringify({ error: 'Could not connect to rate limiter' }), { status: 502 });
+    }
+
     // Extract useful information
-    const ip = headers.get('cf-connecting-ip');
-    const country = headers.get('cf-ipcountry');
+    const country = request.headers.get('cf-ipcountry');
     // Get Cloudflare object
     const { city, region, asn, asOrganization, timezone } = request.cf || {};
     // Build useful AS information
@@ -42,3 +52,24 @@ export default {
     return new Response(JSON.stringify(payload, null, 2));
   },
 };
+
+export class RateLimiter implements DurableObject {
+  static readonly milliseconds_per_request = 1;
+  static readonly milliseconds_for_grace_period = 1000;
+
+  nextAllowedTime: number;
+
+  constructor(_state: DurableObjectState, _env: Env) {
+    this.nextAllowedTime = 0;
+  }
+
+  async fetch(_request: Request): Promise<Response> {
+    const now = Date.now();
+
+    this.nextAllowedTime = Math.max(now, this.nextAllowedTime);
+    this.nextAllowedTime += RateLimiter.milliseconds_per_request;
+
+    const value = Math.max(0, this.nextAllowedTime - now - RateLimiter.milliseconds_for_grace_period);
+    return new Response(JSON.stringify({ milliseconds_to_next_request: value }));
+  }
+}
